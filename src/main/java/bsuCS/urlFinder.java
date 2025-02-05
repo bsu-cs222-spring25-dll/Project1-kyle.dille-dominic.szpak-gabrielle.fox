1a3bb7b7b94f8986e5b761ea5df3ead9a51c9ac2
corl87 <kyledille87@gmail.com>
2025-02-05 15:42:50 -0500

    Passing Unit Test

--- src/main/java/bsuCS/urlFinder.java
+++ src/main/java/bsuCS/urlFinder.java
@@ -1,9 +1,19 @@
 package bsuCS;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 public class urlFinder {
-    public String getapi(String s) {
-        String apiCall = String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles="
-                + s + "&formatversion=2&rvprop=timestamp%7Cuser&rvslots=*&rvlimit=21");
-        return apiCall;
+    public static URL getapi(String title) throws IOException {
+
+        String encodedTitle = URLEncoder.encode(title, StandardCharsets.UTF_8);
+
+        String apiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles="
+                + encodedTitle + "&formatversion=2&rvprop=timestamp%7Cuser&rvslots=*&rvlimit=21";
+
+        // Create URL object
+        URL url = new URL(apiUrl);
+        return(url);
     }
 }
--- src/test/java/bsuCS/testApiUrl.java
+++ src/test/java/bsuCS/testApiUrl.java
@@ -1,22 +1,20 @@
 package bsuCS;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class testApiUrl {
-    @Test
-    public void passingURLTest(){
-        String testURL = "https://en.wikipedia.org/w/api.php" +
-                "?action=query&format=json&prop=revisions" +
-                "&titles=Shaandaar&formatversion=2&rvprop=timestamp%7Cuser&rvslots=*&rvlimit=21";
-        urlFinder urlFinder = new urlFinder();
-        String apiUrl = urlFinder.getapi("Shaandaar");
-        assertEquals(testURL,apiUrl);
+        @Test
+        public void passingURLTest() throws IOException {
+            String rawURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Shaandaar"
+                    + "&formatversion=2&rvprop=timestamp%7Cuser&rvslots=*&rvlimit=21";
+            URL formattedURL = new URL(rawURL);
+            URL trialAPIURL = urlFinder.getapi("Shaandaar");
+            assertEquals(formattedURL,trialAPIURL);
+        }
     }
 
-
-
-
-}
