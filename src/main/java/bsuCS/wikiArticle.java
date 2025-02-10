54fe11efdd19eb85754c6cf421623e21ec50d6ec
corl87 <kyledille87@gmail.com>
2025-02-10 18:29:49 -0500

    Refactoring and redirects

--- src/main/java/bsuCS/wikiArticle.java
+++ src/main/java/bsuCS/wikiArticle.java
@@ -7,12 +7,16 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class wikiArticle {
-    private JSONArray timestamps;
     public static JSONArray pullTimestamps(InputStream testDataStream) throws IOException {
-        JSONArray timestamps = (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
-        return timestamps;
+        JSONArray timestampsArray = (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
+        return timestampsArray;
     }
-    public static void pullRevisionUser(InputStream testDataStream) throws IOException{
-        JSONArray users = (JSONArray) JsonPath.read(testDataStream, "$..user");
+    public static JSONArray pullRevisionUser(InputStream testDataStream) throws IOException{
+        JSONArray usersArray = (JSONArray) JsonPath.read(testDataStream, "$..user");
+        return usersArray;
+    }
+    public static String pullRedirects(InputStream testDataStream) throws IOException {
+        String redirectString = JsonPath.read(testDataStream,"$..to").toString();
+        return redirectString;
     }
 }
