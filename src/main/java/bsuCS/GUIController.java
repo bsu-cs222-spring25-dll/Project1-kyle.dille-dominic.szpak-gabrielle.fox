7a8d7c79b1fea703d09c6eff7efb4fbdc3d2e8fb
corl87 <kyledille87@gmail.com>
2025-02-13 16:13:49 -0500

    GUI compiles

---
+++ src/main/java/bsuCS/GUIController.java
@@ -0,0 +1,4 @@
+package bsuCS;
+
+public class GUIController {
+}
--- src/main/java/bsuCS/GraphicalUserInterface.java
+++ src/main/java/bsuCS/GraphicalUserInterface.java
@@ -1,6 +1,5 @@
 package bsuCS;
 
-import static javafx.application.Application.*;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,12 +7,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class GraphicalUserInterface extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("test");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(new javafx.scene.control.Label("foo"));
         primaryStage.show();
     }
     public static void main(String[] args) {launch(args);}
